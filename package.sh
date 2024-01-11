58a274fecb382028ae312358e73308c781c549a2
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2024-01-11 17:47:58 +0100

    update package script

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 ./version-bump.sh
 cd ..
 zip -r genealogy-profiles/genealogy-profiles-$(date '+%Y%m%d').zip\
-	genealogy-profiles/latex/*.{sty,csv,tex}\
+	genealogy-profiles/latex/genealogy-profiles.sty\
 	genealogy-profiles/docs/*.{tex,pdf,bib,txt,sty}\
 	genealogy-profiles/README.md
 cd -
